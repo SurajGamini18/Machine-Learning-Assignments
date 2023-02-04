31baff3cc4c5b6699117cc031b989d50f702b715
SurajGamini18 <122587886+SurajGamini18@users.noreply.github.com>
2023-02-03 22:24:49 -0600

    added assignment 2 codes

---
+++ Assignment- 2 src/Question2.py
@@ -0,0 +1,4 @@
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# start from index 1 with 2 steps in between
+for i in my_list[1::2]:
+    print(i, end=" ")
---
+++ Assignment- 2 src/Question3.py
@@ -0,0 +1,6 @@
+x = [23, 'Python', 23.98]
+types = [] # creating an empty list
+for i in x:
+    types.append(type(i)) # using type function
+print(x)
+print(types)
